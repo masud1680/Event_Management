e240b790b00f9198c17800a6eeee7441c8af93e2
Md Masud <masudhasan1680@gmail.com>
2025-09-10 15:44:54 +0600

    resolve fontend url

--- event_management/settings.py
+++ event_management/settings.py
@@ -169,4 +169,4 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
-FONTEND_URL = 'https://*.onrender.com'
+FONTEND_URL = 'https://event-management-2-hwd7.onrender.com'
